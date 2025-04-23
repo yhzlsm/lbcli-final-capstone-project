c52f9b54c3af68f1edb2698f25b105de1cac1fda
yhzlsm <yhzlsm@gmail.com>
2025-04-23 15:11:19 -0300

    fix: change the block number

--- submission/02.sh
+++ submission/02.sh
@@ -1,5 +1,5 @@
 # How many new outputs were created by block 243,825?
-block_hash=$(bitcoin-cli -signet getblockhash "243833")
+block_hash=$(bitcoin-cli -signet getblockhash "243825")
 block=$(bitcoin-cli -signet getblock "$block_hash" 2)
 transactions=$(echo $block | jq '.tx')
 count=$(echo $transactions | jq '[.[].vout | length] | add')
