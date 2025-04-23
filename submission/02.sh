8f300473ee68387cd0e9f215aaf71063f3df3e0f
yhzlsm <yhzlsm@gmail.com>
2025-04-23 14:55:47 -0300

    feat: add count outputs in block 243825

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,11 @@
 # How many new outputs were created by block 243,825?
+block_hash=$(bitcoin-cli -signet getblockhash "243833")
+block=$(bitcoin-cli -signet getblock "$block_hash" 2)
+transactions=$(echo $block | jq '.tx')
+vouts=$(echo $transactions | jq '.[].vout')
+count=0
+for i in $vouts
+do
+  count=$((count + 1))
+done
+echo $count
