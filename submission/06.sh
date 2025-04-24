ad5cd31ba9e54f8a146870eb3d2b06e58582fbe6
yhzlsm <yhzlsm@gmail.com>
2025-04-24 09:37:08 -0300

    feat: add rbf transaction id

--- submission/06.sh
+++ submission/06.sh
@@ -1,1 +1,10 @@
 # Only one tx in block 243,821 signals opt-in RBF. What is its txid?
+block_hash=$(bitcoin-cli -signet getblockhash "243821")
+block=$(bitcoin-cli -signet getblock "$block_hash" 2)
+echo "$block" | jq -r '.tx[] | .txid' | while read txid; do
+  tx=$(bitcoin-cli -signet getrawtransaction "$txid" 1)
+  is_rbf=$(echo "$tx" | jq 'any(.vin[]; .sequence < 4294967294)')
+  if [ "$is_rbf" = "true" ]; then
+    echo "$txid"
+  fi
+done
