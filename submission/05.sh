6ce22f06d4b8b44c006575e5d5b6850fbbd9c8fc
yhzlsm <yhzlsm@gmail.com>
2025-04-24 09:23:15 -0300

    feat: add fee value

--- submission/05.sh
+++ submission/05.sh
@@ -1,1 +1,6 @@
 # How many satoshis did this transaction pay for fee?: b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
+transaction=$(bitcoin-cli -signet getrawtransaction "b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb" 2)
+amount_vout=$(echo "$transaction" | jq -r ".vout | map(.value * 100000000) | add")
+amount_vin=$(echo "$transaction" | jq -r ".vin | map(.prevout.value * 100000000) | add")
+fee=$(echo "$amount_vin - $amount_vout" | bc)
+echo "$fee"
