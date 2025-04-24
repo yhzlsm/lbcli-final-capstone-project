aad03f0223cfd0fab1e28b83ff7b7b1183d76fb0
yhzlsm <yhzlsm@gmail.com>
2025-04-24 10:07:57 -0300

    feat: add coinbase tx

--- submission/07.sh
+++ submission/07.sh
@@ -1,1 +1,4 @@
 # what is the coinbase tx in this block 243,834
+block_hash=$(bitcoin-cli -signet getblockhash "243834")
+block=$(bitcoin-cli -signet getblock "$block_hash" 2)
+echo "$block" | jq -r '.tx[0].txid'
