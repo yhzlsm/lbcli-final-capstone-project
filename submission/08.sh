f4dc865e16e5ac031adb06409ba4d465b4b7632e
yhzlsm <yhzlsm@gmail.com>
2025-04-24 10:11:26 -0300

    feat: add the real block transaction

--- submission/08.sh
+++ submission/08.sh
@@ -1,5 +1,5 @@
 # what block height was this tx mined ?
-# 49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb
-block_hash=$(bitcoin-cli -signet getblockhash "243834")
+transaction=49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb
+block_hash=$(bitcoin-cli -signet getrawtransaction $transaction 1 | jq -r '.blockhash')
 block_header=$(bitcoin-cli -signet getblockheader "$block_hash")
 echo "$block_header " | jq -r '.height'
