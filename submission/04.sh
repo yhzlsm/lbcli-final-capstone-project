04c5357d84b621ead590bc5673667d09f4bb8291
yhzlsm <yhzlsm@gmail.com>
2025-04-24 09:09:22 -0300

    feat: add publickey from witness

--- submission/04.sh
+++ submission/04.sh
@@ -1,1 +1,4 @@
 # Which public key signed input 0 in this tx: d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7
+transaction=$(bitcoin-cli -signet getrawtransaction "d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7" 1)
+public_key=$(echo $transaction | jq -r '.vin[0].txinwitness[1]')
+echo $public_key
